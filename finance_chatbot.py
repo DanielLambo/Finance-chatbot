0f63d6771f3967dac3c4d8bb218f9b8305d45464
DanielLambo <146047658+DanielLambo@users.noreply.github.com>
2025-02-07 08:32:33 -0600

    Update finance_chatbot.py

--- finance_chatbot.py
+++ finance_chatbot.py
@@ -12,7 +12,7 @@ import google.generativeai as genai
 load_dotenv()
 #gemini_api_key = os.getenv("gemini_api_key")
 if not os.getenv("gemini_api_key"):
-    os.environ["gemini_api_key"] = "your_api_key_here
+    os.environ["gemini_api_key"] = "AIzaSyAIY290yIq-zhPMWZnmmr3lekuKpHk0gs0"
     # Set up the Streamlit app
 st.title('Ask Me Anything About The Cozy-Corner BNB')
 st.image('Flux_Dev_create_a_2d_cartoon_image_of_a_confident_and_professi_3.jpeg', width=300,)
