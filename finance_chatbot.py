77f6f121cdff9b5f207d87cc28ba94170d28fcd5
DanielLambo <146047658+DanielLambo@users.noreply.github.com>
2025-02-10 01:09:49 -0600

    Update finance_chatbot.py

--- finance_chatbot.py
+++ finance_chatbot.py
@@ -10,7 +10,6 @@ import google.generativeai as genai
 
 load_dotenv()
 gemini_api_key = os.getenv("gemini_api_key")
-st.write(gemini_api_key)
 
 if not gemini_api_key:
     gemini_api_key = st.secrets["gemini_api_key"]  # Access the secret
